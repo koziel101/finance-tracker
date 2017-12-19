9733d726b57e6a0f39bae72e1ba11f0399dc509c
Hiago Koziel <hiagokoziel100@gmail.com>
2017-12-19 21:59:22 +0000

    Add first and last name to registration for new users with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  devise_for :users, :controllers => { :registrations => "users/registrations"}
+  resources :user_stocks, only: [:create, :destroy]
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create, :destroy]
   
 end
